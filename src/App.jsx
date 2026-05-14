257db75532ff7610e9792abdecd3960ba05f55ef
maury1999 <maurici.gutierrez.o1@gmail.com>
2026-05-13 23:52:45 -0300

    Coreccion de app.jsx Mauricio Gutierrez

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* PUNTO 3: Barra de navegación subida por tu compañero */}
       <Nav />
       
       <main className="container mt-4">
-        {/* Título principal solicitado en el layout */}
         <h1 className="text-center mb-4">Gestión de Proyectos Educativos</h1>
-        
-        {/* PUNTO 4: Tu componente de listado, búsqueda y borrado */}
         <ListaProyectos />
       </main>
     </div>
