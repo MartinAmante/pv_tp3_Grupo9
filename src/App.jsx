6757d3cd97bc4d204e6ad4ea144b7dd1ad596490
TomasVilte <tomiviltej7@gmail.com>
2026-05-20 19:42:01 -0300

    agregando DetalleProyecto.jsx y modificando proyectoService.js Tomas Vilte

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Nav from './components/Nav';
 import Header from './components/Header';
 import FormularioProyecto from './components/FormularioProyecto';
 import ListaProyectos from './components/ListaProyectos';
+import DetalleProyecto from './components/DetalleProyecto';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   const [proyectos, setProyectos] = useState(obtenerProyectos());
+  const [proyectoSeleccionado, setProyectoSeleccionado] = useState(null);
 
   return (
     <div className="App d-flex flex-column min-vh-100">
@@ -20,7 +22,8 @@ function App() {
         
         <hr className="my-5" />
         
-        <ListaProyectos proyectosState={proyectos} setProyectos={setProyectos} />
+        <ListaProyectos proyectosState={proyectos} setProyectos={setProyectos}  setProyectoSeleccionado={setProyectoSeleccionado} />
+        <DetalleProyecto proyecto={proyectoSeleccionado} />
       </main>
       <Footer />
     </div>
