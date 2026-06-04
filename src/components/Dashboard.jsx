d8d505aa69fc1a1a5e946b6c57010d82138e43da
GalianFernando <handpek23@gmail.com>
2026-06-04 14:22:32 -0300

    agtualizacion del branch

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,10 +5,7 @@ const Dashboard = () => {
             <h2>Dashboard</h2>
             <p>Bienvenido al sistema de gestión de proyectos.</p>
             <p>En esta pagina se le brindara la capasidad de cargar proyectos en nuestra plataforma </p>
-
-
             <br/>  
-
             <div className="card shadow-sm mb-3 border-primary">   
                 <div className="card-body">
                     <h3 className="card-title text-primary">Métricas Generales</h3>
@@ -19,7 +16,6 @@ const Dashboard = () => {
                     </ul>
                 </div>
             </div>
-
             <br/>  
         </div>
     );
--- src/components/PerfilUsuario.jsx
+++ src/components/PerfilUsuario.jsx
@@ -3,19 +3,12 @@ const PerfilUsuario = () => {
     return (
         <div className="card shadow-sm mb-3 border-primary p-4">
             <h2>Perfil de Profesor </h2>
-            <p>Nombre: Gustvo Sosa</p>
+            <p>Nombre: Gustvo Sosa </p>
             <p>Rol: Profesor</p>
             <p>Institución: Facultad de Ingeniería</p>
-            <p></p>
         </div>
     );
 };
 
 export default PerfilUsuario;
 
-/*
-  2. <PerfilUsuario /> (src/views/PerfilUsuario.jsx): Pantalla del perfil del usuario 
-  (/perfil). Debe simular los datos del alumno o docente (Nombre, Rol, Institución) 
-  organizados elegantemente dentro de un componente Paper o una lista de Material 
-  UI.
-*/
