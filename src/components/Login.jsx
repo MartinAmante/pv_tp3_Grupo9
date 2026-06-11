0de982a8f41a08bdd67bfdb905e28edbd2db4b5e
GalianFernando <handpek23@gmail.com>
2026-06-11 02:38:52 -0300

    agrego login y validarService - Fernando Galian

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,27 +3,39 @@ import { UsuarioContext } from '../context/UsuarioContext';
 
 const Login = () => {
 
-    const [datosUsuario, setDatosUsuario] = useState({
-        nombre: '',
-        contraseña: ''
-    });
-    
-    
-        const handleChange = (e) => {
+   export const guardado = () => {
+    const [guardar, setGuardar] = useState({ });
+}
 
-        
-        }
+    const [nombre, setNombre] = useState('');
+    const [contraseña, setContraseña] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+            const nuevo = {
+                nombre,
+                contraseña
+            };
+        setGuardar(nuevo);
+           setNombre('')
+           setContraseña('')
+           }
 
 
     return (
             <div className="card shadow-sm mb-3 border-primary " >   
               <h2>Iniciar Sesión</h2>
-            <form  >
+            <form onSubmit={handleSubmit}   >
                 <div>
                     <label>Nombre: </label>
                     <input
                         type="text"
                         name="nombre"
+                        value= {nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                        required
                     />
                     
                 </div>
@@ -33,14 +45,19 @@ const Login = () => {
                     <input
                         type="password"
                         name="contraseña"
-                                        
+                        value= {contraseña}
+                        onChange={(e) => setContraseña(e.target.value)}
+                        required
                     />
                 
                 </div>
                 <br />
-                <button type="submit">Iniciar Sesión</button>
+                <button type="submit" 
+                
+                >Iniciar Sesión</button>
                 
             </form>
+
         </div>
     );
 };
